d4f7d3441d67e9d7b5e2a2e891e755423bfb12bd
Noor Hannan <1211104293@student.mmu.edu.my>
2025-06-18 00:35:28 +0800

    include login option for the unlogged in

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,6 +1,9 @@
 </main>
 <footer>
     <div class="sidebar">
+        <?php if (!isset($_SESSION['user_id'])): ?>
+            <a href="login.php">Sign in</a>
+        <?php endif; ?>
         <a href="student_dashboard.php">🏠 Home</a>
         <a href="catalog.php">🎨 Catalog</a>
         <a href="profile.php">😊 Profile</a>
